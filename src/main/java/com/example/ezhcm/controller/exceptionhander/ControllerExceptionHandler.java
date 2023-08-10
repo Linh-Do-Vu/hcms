085387d3bbcffc87224d2d8d9240001a975e132a
LinhDoVu <vulinh102ii@gmail.com>
2023-08-10 14:21:04 +0700

    fix name api and add search document project

--- src/main/java/com/example/ezhcm/controller/exceptionhander/ControllerExceptionHandler.java
+++ src/main/java/com/example/ezhcm/controller/exceptionhander/ControllerExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import java.util.Date;
 
-@RestControllerAdvice(value = "com.example.hospital8.controller")
+@RestControllerAdvice(value = "com.example.ezhcm.controller")
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = CustomException.class)
     public ResponseEntity<?> handleSelfServiceException(CustomException e, WebRequest webRequest) {
